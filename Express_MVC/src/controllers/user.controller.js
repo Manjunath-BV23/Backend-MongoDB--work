3c6450a072110222154645aed0a331f0ed91a184
Manjunath B V <76251902+Manjunath-BV23@users.noreply.github.com>
2024-11-07 17:59:57 +0530

    formatted

--- Express_MVC/src/controllers/user.controller.js
+++ Express_MVC/src/controllers/user.controller.js
@@ -13,21 +13,21 @@ router.post("", async (req, res) => {
       } catch (err) {
         return res.status(500).send(err.message);
       }
-    });
+});
     
-    router.get("", async (req, res) => {
+router.get("", async (req, res) => {
       // thennable => proper then
       try {
         const users = await User.find().lean().exec(); // db.users.find() // proper promise
-    
+      
         return res.send(users);
       } catch (err) {
         return res.status(500).send(err.message);
       }
-    });
+});
     
     // met + route => get /users/${variable} and the name of variable is id
-    router.get("/:id", async (req, res) => {
+router.get("/:id", async (req, res) => {
       try {
         const user = await User.findById(req.params.id).lean().exec();
     
@@ -39,10 +39,10 @@ router.post("", async (req, res) => {
       } catch (err) {
         return res.status(500).send(err.message);
       }
-    });
+});
     
     // met + routeusers/${variable} and the name of  => patch /variable is id
-    router.patch("/:id", async (req, res) => {
+router.patch("/:id", async (req, res) => {
       try {
         const user = await User.findByIdAndUpdate(req.params.id, req.body, {
           new: true,
@@ -54,10 +54,10 @@ router.post("", async (req, res) => {
       } catch (err) {
         return res.status(500).send(err.message);
       }
-    });
+});
     
     // met + route => delete /users/${variable} and the name of variable is id
-    router.delete("/:id", async (req, res) => {
+router.delete("/:id", async (req, res) => {
       try {
         const user = await User.findByIdAndDelete(req.params.id).lean().exec();
     
@@ -65,6 +65,6 @@ router.post("", async (req, res) => {
       } catch (err) {
         return res.status(500).send(err.message);
       }
-    });
+});
 
-    module.exports = router
+module.exports = router
